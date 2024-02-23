5c9a7f1214ff775c7847ef792aad4a95e30a989b
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2024-02-23 18:06:24 +0100

    Proper validation

--- inc/get.php
+++ inc/get.php
@@ -3,11 +3,11 @@
 <div class="form-container">
     <form action="" method="post">
         <label for="name">Name</label>
-        <input type="text" name="name" id="name" placeholder="John Doe" >
+        <input type="text" name="name" id="name" placeholder="John Doe" required>
         <small style="color: red;"><?php if (isset($ERROR["name"]))
             echo $ERROR["name"]; ?></small>
         <label for="email">Email</label>
-        <input type="email" name="email" id="email" placeholder="example@mail.com" >
+        <input type="email" name="email" id="email" placeholder="example@mail.com" required>
         <small style="color:red;"><?php if (isset($ERROR["email"]))
             echo $ERROR["email"]; ?></small>
 
--- inc/post.php
+++ inc/post.php
@@ -2,13 +2,21 @@
 <?php
 
 $ERROR = array();
-$ERROR['name'] = 'Name is required';
-$ERROR['email'] = 'Email is required';
 
-$name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_STRING);
+$name = trim(filter_input(INPUT_POST, "name", FILTER_SANITIZE_STRING));
 $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
 
-if (empty($name) || empty($email) || empty($email)) {
+if (empty($name)) {
+    $ERROR['name'] = 'Name is required';
+}
+if (empty($email)) {
+    $ERROR['email'] = 'Email is required';
+}
+if (filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
+    $ERROR['email'] = "Email is not valid";
+}
+
+if (count($ERROR) > 0) {
     include __DIR__ . '/get.php';
 } else {
     echo "<h1>Confirmation mail sent to " . $email . "</h1>";
